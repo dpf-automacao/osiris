8964cfddeafc905e173216aa048ec708ef541dd2
Alexandre Pereira Santos <alex.endrios@hotmail.com>
2018-12-12 15:46:18 -0200

    setup - ambiente

---
+++ features/support/env.rb
@@ -0,0 +1,30 @@
+require "selenium-webdriver"
+require "capybara"
+require 'capybara/cucumber'
+require "rspec"
+require 'site_prism'
+
+
+
+Capybara.register_driver :insecure_selenium do |app|
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :firefox,
+    desired_capabilities: { accept_insecure_certs: true }
+  )
+
+end
+
+Capybara.configure do |config|
+  config.run_server = false
+  config.default_driver = :insecure_selenium
+
+  config.app_host = 'https://sistemasdesenv.dpf.gov.br/siscaer-intranet-web'
+
+ 
+  #'https://prehom.dpf.gov.br/siscaer-intranet-web/'
+end
+
+Capybara.default_max_wait_time = 20
+
+#Capybara.page.driver.browser.manage.window.maximize
