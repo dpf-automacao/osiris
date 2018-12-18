68145c9359f9d35f0a0cb1e8ec11b850a5c0be38
Alexandre Pereira Santos <alex.endrios@hotmail.com>
2018-12-18 11:38:35 -0200

    update

--- features/step_definitions/captura_sonar.rb
+++ features/step_definitions/captura_sonar.rb
@@ -3,17 +3,17 @@ Dado("que gostaria de visualizar o sonar") do
     #setup miner
     miner_sonar = CapturaSonar.new
     url_miner_sonar = 'https://sistemas.dpf.gov.br/sonar/dashboard?id=br.gov.dpf%3Aminer-rest'
-    miner_sistema = 'MINER'
+    miner_sistema = 'MINER-REST'
 
     #setup siscaer
     siscaer_sonar = CapturaSonar.new
     url_siscaer_sonar = 'https://sistemas.dpf.gov.br/sonar/dashboard?id=br.gov.dpf%3Asiscaer-batch'
-    siscaer_sistema = 'SISCAER'
+    siscaer_sistema = 'SISCAER-BATCH'
     
     #setup siproquim
     siproquim_sonar = CapturaSonar.new
     url_siproquim_sonar = 'https://sistemas.dpf.gov.br/sonar/dashboard?id=br.gov.dpf%3Asiproquim-fiscalizacao-rest'
-    siproquim_sistema = 'SIPROQUIM'
+    siproquim_sistema = 'SIPROQUIM-FISCALIZACAO-REST'
 
     #setup ferramentas
     salvar = Tools.new
@@ -25,7 +25,7 @@ Dado("que gostaria de visualizar o sonar") do
  
     #salvando arquivo csv
     sonar = 'sonar.csv'
-    info = "#{dados_miner}\n#{dados_siscaer}\n#{dados_siproquim}"
+    info = "PROJETOS;BUGS;VULNERABILIDADES;CODE SMELLS;COBERTURA;DUPLICADOS\n#{dados_miner}\n#{dados_siscaer}\n#{dados_siproquim}"
     salvar.salvar_dados(sonar, info)
 
 end
